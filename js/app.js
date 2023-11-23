5f8f5d68f08da0a514960570007597215b5b5317
Fumiko Toyoda <toyodafumiko@gmail.com>
2023-11-23 20:42:44 +0900

    store messages to firebase

--- js/app.js
+++ js/app.js
@@ -1,104 +1,125 @@
 // FirebaseのSDKをimport
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
-import { getDatabase, ref, push, set, onChildAdded } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  set,
+  onChildAdded,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
 
 // Firebaseの設定
-// const firebaseConfig = {
-    // 使う時はここにfirebaseの設定を入れる
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyBWBuPcufMg9sY1rx_3n4U7QU2hMfyE6Gw",
+  authDomain: "faqapp202311.firebaseapp.com",
+  projectId: "faqapp202311",
+  storageBucket: "faqapp202311.appspot.com",
+  messagingSenderId: "950282451735",
+  appId: "1:950282451735:web:b1228191e3408f41e55552",
+};
 
 // Firebaseの初期化
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const dbRef = ref(db, "chat");
 
-// データ送信のイベントリスナー
-document.getElementById("send-btn").addEventListener("click", function () {
-    const text = document.getElementById("text").value;
-    const msg = { text };
-    const newPostRef = push(dbRef);
-    set(newPostRef, msg);
-
-    document.getElementById("text").value = "";
-});
-
-// データ取得と表示
 onChildAdded(dbRef, function (data) {
-    const msg = data.val();
-    const key = data.key;
-
-    let html = `<div class="${key}">
-                    <p>${msg.text}</p>
-                </div>`;
-
-    document.getElementById("output").innerHTML += html;
+  const msg = data.val();
+  const className = msg.source === 'user' ? 'outgoing' : 'incoming';
+  chatbox.appendChild(createChatLi(msg.text, className));
+  chatbox.scrollTo(0, chatbox.scrollHeight);
 });
 
-// ここから下。AIチャットぼっとアプリのコード - 最終的にこれを合体させて、AIチャットbotの記録をfirebaseに保存する
+// ここから下。AIチャットボットアプリのコード 
 const chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 let userMessage = null; // Variable to store user's message
-// const API_KEY = 使う時はここにAPIキーを入れる
+const API_KEY = "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";
 const inputInitHeight = chatInput.scrollHeight;
 const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> element
-}
+  // Create a chat <li> element with passed message and className
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+  let chatContent =
+    className === "outgoing"
+      ? `<p></p>`
+      : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi; // return chat <li> element
+};
 const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = chatElement.querySelector("p");
-    // Define the properties and message for the API request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
-    }
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  // Define the properties and message for the API request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+    }),
+  };
+  // Send POST request to API, get response and set the reponse as paragraph text
+  fetch(API_URL, requestOptions)
+    .then((res) => res.json())
+    .then((data) => {
+      const aiResponse = data.choices[0].message.content.trim();
+
+      // Save AI response to Firebase
+      const aiResponseRef = push(dbRef);
+      set(aiResponseRef, { text: aiResponse, source: 'ai' });
+      const tempThinking = document.getElementById("temp-thinking");
+      if (tempThinking) {
+        tempThinking.remove();
+      }
+    })
+    .catch(() => {
+      console.error("Error with OpenAI API:", error);
+    })
+};
+
 const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
-    // Clear the input textarea and set its height to default
-    chatInput.value = "";
-    chatInput.style.height = `${inputInitHeight}px`;
-    // Append the user's message to the chatbox
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-    chatbox.scrollTo(0, chatbox.scrollHeight);
-    
-    setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
-        chatbox.appendChild(incomingChatLi);
-        chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
-    }, 600);
-}
+  userMessage = chatInput.value.trim();
+  if (!userMessage) return;
+  chatInput.value = "";
+  chatInput.style.height = `${inputInitHeight}px`;
+
+  // Save user message to Firebase
+  const newPostRef = push(dbRef);
+  set(newPostRef, { text: userMessage, source: 'user' });
+
+  // Create and append temporary "Thinking..." message
+  const tempChatLi = createChatLi("考え中...", "incoming");
+  tempChatLi.id = "temp-thinking"; // Assign an ID for easy removal
+  chatbox.appendChild(tempChatLi);
+  chatbox.scrollTo(0, chatbox.scrollHeight);
+
+  // Generate AI response
+  generateResponse(tempChatLi);
+
+  // setTimeout(() => {
+  //   const incomingChatLi = createChatLi("考え中...", "incoming");
+  //   chatbox.appendChild(incomingChatLi);
+  //   chatbox.scrollTo(0, chatbox.scrollHeight);
+  //   generateResponse(incomingChatLi);
+  // }, 600);
+};
+
 chatInput.addEventListener("input", () => {
-    // Adjust the height of the input textarea based on its content
-    chatInput.style.height = `${inputInitHeight}px`;
-    chatInput.style.height = `${chatInput.scrollHeight}px`;
+  chatInput.style.height = `${inputInitHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
 
 sendChatBtn.addEventListener("click", handleChat);
-closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+closeBtn.addEventListener("click", () =>
+  document.body.classList.remove("show-chatbot")
+);
+chatbotToggler.addEventListener("click", () =>
+  document.body.classList.toggle("show-chatbot")
+);
